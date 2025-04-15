e5c3e28fcebbbe66448714efdf71153342732833
BrunoSilva-code <bfbps@iscte-iul.pt>
2025-04-15 22:40:37 +0100

    Nova bd

--- pisid_sql.sql
+++ pisid_sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Mar-2025 às 17:15
+-- Tempo de geração: 15-Abr-2025 às 23:39
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `pisis`
+-- Banco de dados: `pisid_sql`
 --
 
 -- --------------------------------------------------------
@@ -65,8 +65,17 @@ CREATE TABLE `jogo` (
   `IDJogo` int(11) NOT NULL,
   `Descricao` text NOT NULL,
   `jogador` int(11) NOT NULL,
-  `DataHorainicio` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `DataHorainicio` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Estado` varchar(20) NOT NULL,
+  `Score` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `jogo`
+--
+
+INSERT INTO `jogo` (`IDJogo`, `Descricao`, `jogador`, `DataHorainicio`, `Estado`, `Score`) VALUES
+(1, '', 22, '2025-04-12 00:17:57', '', 0);
 
 -- --------------------------------------------------------
 
@@ -84,6 +93,16 @@ CREATE TABLE `medicoespassagens` (
   `IDJogo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Extraindo dados da tabela `medicoespassagens`
+--
+
+INSERT INTO `medicoespassagens` (`IDMedicao`, `Hora`, `SalaOrigem`, `SalaDestino`, `Marsami`, `Status`, `IDJogo`) VALUES
+(1, '2025-04-15 21:02:07', 3, 6, 50, 1, 1),
+(7, '2025-04-15 21:02:09', 3, 6, 62, 1, 1),
+(13, '2025-04-15 21:08:34', 3, 6, 46, 1, 1),
+(19, '2025-04-15 21:08:37', 3, 6, 45, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -96,11 +115,18 @@ CREATE TABLE `mensagens` (
   `Sala` int(11) NOT NULL,
   `Sensor` int(11) NOT NULL,
   `Leitura` decimal(6,2) NOT NULL,
-  `TipoAlerta` varchar(50) NOT NULL,
+  `TipoAlerta` int(11) NOT NULL,
   `Msg` varchar(100) NOT NULL,
   `HoraEscrita` timestamp NULL DEFAULT current_timestamp(),
   `IDJogo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `mensagens`
+--
+
+INSERT INTO `mensagens` (`ID`, `Hora`, `Sala`, `Sensor`, `Leitura`, `TipoAlerta`, `Msg`, `HoraEscrita`, `IDJogo`) VALUES
+(1, '2025-04-15 18:09:29', 3, 3, 22.00, 1, 'Ola testes', '2025-04-12 00:19:34', 1);
 
 -- --------------------------------------------------------
 
@@ -115,6 +141,13 @@ CREATE TABLE `ocupacaolabirinto` (
   `Sala` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Extraindo dados da tabela `ocupacaolabirinto`
+--
+
+INSERT INTO `ocupacaolabirinto` (`IDJogo`, `NumeroMarsamisOdd`, `NumeroMarsamisEven`, `Sala`) VALUES
+(1, 1, 13, 6);
+
 -- --------------------------------------------------------
 
 --
@@ -126,21 +159,16 @@ CREATE TABLE `setupmaze` (
   `numberrooms` int(11) NOT NULL,
   `numbermarsamis` int(11) NOT NULL,
   `numberplayers` int(11) NOT NULL,
-  `frozentime` int(11) NOT NULL,
-  `delaytime` int(11) NOT NULL,
-  `timemarsamilive` int(11) NOT NULL,
   `noisevartoleration` decimal(5,2) NOT NULL,
-  `step` int(11) NOT NULL,
-  `ID` int(11) NOT NULL,
-  `minutesstep` decimal(3,2) DEFAULT NULL
+  `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Extraindo dados da tabela `setupmaze`
 --
 
-INSERT INTO `setupmaze` (`normalnoise`, `numberrooms`, `numbermarsamis`, `numberplayers`, `frozentime`, `delaytime`, `timemarsamilive`, `noisevartoleration`, `step`, `ID`, `minutesstep`) VALUES
-(19.00, 10, 30, 40, 50, 3, 50, 2.50, 50, 0, 1.00);
+INSERT INTO `setupmaze` (`normalnoise`, `numberrooms`, `numbermarsamis`, `numberplayers`, `noisevartoleration`, `ID`) VALUES
+(19.00, 10, 30, 40, 2.50, 0);
 
 -- --------------------------------------------------------
 
@@ -154,6 +182,51 @@ CREATE TABLE `sound` (
   `Sound` varchar(12) NOT NULL,
   `IdJogo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `sound`
+--
+
+INSERT INTO `sound` (`IDSound`, `Hour`, `Sound`, `IdJogo`) VALUES
+(1, '2025-03-08 12:15:32', '32', 1),
+(2, '2025-03-08 12:16:45', '27', 1),
+(3, '2025-03-08 12:17:21', '45', 1),
+(4, '2025-03-08 12:18:09', '19', 1),
+(5, '2025-03-08 12:19:38', '50', 1),
+(6, '2025-03-08 12:20:12', '38', 1),
+(7, '2025-03-08 12:21:47', '22', 1),
+(8, '2025-03-08 12:22:54', '29', 1),
+(9, '2025-03-08 12:23:18', '41', 1),
+(10, '2025-03-08 12:24:05', '35', 1),
+(11, '2025-03-08 12:25:33', '20', 1),
+(12, '2025-03-08 12:26:22', '47', 1),
+(13, '2025-03-08 12:27:19', '39', 1),
+(14, '2025-03-08 12:28:41', '28', 1),
+(15, '2025-03-08 12:29:10', '30', 1),
+(16, '2025-03-08 12:30:02', '44', 1),
+(17, '2025-03-08 12:31:15', '26', 1),
+(18, '2025-03-08 12:32:29', '33', 1),
+(19, '2025-03-08 12:33:55', '37', 1),
+(20, '2025-03-08 12:34:48', '25', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipoalerta`
+--
+
+CREATE TABLE `tipoalerta` (
+  `IDTipoAlerta` int(11) NOT NULL,
+  `TipoAlerta` varchar(50) NOT NULL,
+  `gravidade` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tipoalerta`
+--
+
+INSERT INTO `tipoalerta` (`IDTipoAlerta`, `TipoAlerta`, `gravidade`) VALUES
+(1, 'Warring', 'High');
 
 -- --------------------------------------------------------
 
@@ -205,13 +278,15 @@ ALTER TABLE `medicoespassagens`
 --
 ALTER TABLE `mensagens`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `chaveestrageira3` (`IDJogo`);
+  ADD KEY `chaveestrageira3` (`IDJogo`),
+  ADD KEY `idtipoalerta` (`TipoAlerta`);
 
 --
 -- Índices para tabela `ocupacaolabirinto`
 --
 ALTER TABLE `ocupacaolabirinto`
   ADD PRIMARY KEY (`IDJogo`),
+  ADD UNIQUE KEY `Sala` (`Sala`),
   ADD KEY `IDJogo` (`IDJogo`);
 
 --
@@ -226,6 +301,12 @@ ALTER TABLE `setupmaze`
 ALTER TABLE `sound`
   ADD PRIMARY KEY (`IDSound`),
   ADD KEY `chaveestrageira2` (`IdJogo`);
+
+--
+-- Índices para tabela `tipoalerta`
+--
+ALTER TABLE `tipoalerta`
+  ADD PRIMARY KEY (`IDTipoAlerta`);
 
 --
 -- Índices para tabela `utilizador`
@@ -276,7 +357,8 @@ ALTER TABLE `medicoespassagens`
 -- Limitadores para a tabela `mensagens`
 --
 ALTER TABLE `mensagens`
-  ADD CONSTRAINT `chaveestrageira3` FOREIGN KEY (`IDJogo`) REFERENCES `jogo` (`IDJogo`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chaveestrageira3` FOREIGN KEY (`IDJogo`) REFERENCES `jogo` (`IDJogo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `idtipoalerta` FOREIGN KEY (`TipoAlerta`) REFERENCES `tipoalerta` (`IDTipoAlerta`) ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `ocupacaolabirinto`
